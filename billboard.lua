b2f4306a63c3d357683acec2e941d0cba2d812fd
Karmatic <Jake@jakecoffey.com>
2026-01-02 23:27:18 -0500

    spellcheck

--- billboard.lua
+++ billboard.lua
@@ -2,7 +2,7 @@ INTEREVAL= 3
 
 TEXT_SCALE = 3
 
-MONITOR_SIDE = "left"
+
 
 TEXT_X_POS = 1
 TEXT="Welcome To DinePart"
@@ -31,14 +31,14 @@ TEXT_COLORS={
  col_indx = 1
 
  function changeCol(monitor)
-    --checls if index is within range
+    --checks if index is within range
      if col_indx > tableLength(BACK_COLORS)then
         col_indx=1
     end
     --sets background and text color
     local backCol = BACK_COLORS(col_indx)
     local textCol = TEXT_COLORS(col_indx)
-    --increments color index
+    --ups color index
     col_indx= col_indx + 1
     --clears last display
     monitor.clear()
@@ -52,7 +52,7 @@ TEXT_COLORS={
 
  end
  function run()
-    local monitor =peripheral.wrap(MONITOR_SIDE)
+    local monitor =peripheral.find("monitor")
     
     while true do 
         changeCol(monitor)
